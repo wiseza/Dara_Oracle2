4b0dfae681d739c6813d4c0575a936c2cd16f99f
poranunb-design <poranun.b@kkumail.com>
2026-03-05 02:25:26 +0700

    Add

--- app/src/main/java/com/example/dara/CalendarFragment.kt
+++ app/src/main/java/com/example/dara/CalendarFragment.kt
@@ -1,15 +1,24 @@
 package com.example.dara
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CalendarFragment : Fragment(R.layout.fragment_calendar) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // ใส่ logic ของหน้านี้ตรงนี้
+
+        val btnBack = view.findViewById<View>(R.id.btnback_cal)
+
+        btnBack?.setOnClickListener {
+            if (parentFragmentManager.backStackEntryCount > 0) {
+                parentFragmentManager.popBackStack()
+            } else {
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.contentContainer, ShowFragment())
+                    .commit()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dara/ShowFragment.kt
+++ app/src/main/java/com/example/dara/ShowFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView // หรือเปลี่ยนเป็น ImageButton ตามประเภทใน XML
 import androidx.fragment.app.Fragment
 
 class ShowFragment : Fragment() {
@@ -14,5 +15,18 @@ class ShowFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_show, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnNext = view.findViewById<View>(R.id.btn_next)
+
+        btnNext.setOnClickListener {
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.contentContainer, CalendarFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
     }
 }
